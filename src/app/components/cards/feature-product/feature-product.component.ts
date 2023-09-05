61c2387468fd062e6865e36eccff059d9071a29a
mitulpatel224 <mitul.patel224@gmail.com>
2023-09-05 19:54:43 +0530

    refactor: feature product card

---
+++ src/app/components/cards/feature-product/feature-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-feature-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-product.component.html',
+  styleUrls: ['./feature-product.component.scss']
+})
+export class FeatureProductComponent {
+
+}
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FeatureProductComponent } from '../cards/feature-product/feature-product.component';
 import { SliderCardComponent } from '../slider/slider-card/slider-card.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, SliderCardComponent],
+  imports: [CommonModule, SliderCardComponent, FeatureProductComponent],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
