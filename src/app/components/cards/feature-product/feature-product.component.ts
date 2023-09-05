e821fa77735baa27cc2c16a3b4e5011aaf57d1f9
mitulpatel224 <mitul.patel224@gmail.com>
2023-09-05 20:32:12 +0530

    feat: feature component with dynamic content and invert option

--- src/app/components/cards/feature-product/feature-product.component.ts
+++ src/app/components/cards/feature-product/feature-product.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-feature-product',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './feature-product.component.html',
-  styleUrls: ['./feature-product.component.scss']
+  styleUrls: ['./feature-product.component.scss'],
 })
 export class FeatureProductComponent {
-
+  @Input() public product?: { image: string; description: string; alt: string };
+  @Input() public invert: boolean = false;
 }
