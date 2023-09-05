f0b2281f2f83968a350202408c93ae6a60da33aa
mitulpatel224 <mitul.patel224@gmail.com>
2023-09-05 18:35:59 +0530

    feat: bootstrap setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent, HeroComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
