2414dc6eeee054404f2ae6693b06a0cddd778ace
mitulpatel224 <mitul.patel224@gmail.com>
2023-09-06 19:08:17 +0530

    feat: advertise 2

--- src/app/components/advertise2/advertise2.component.ts
+++ src/app/components/advertise2/advertise2.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SectionHeaderComponent } from '../section-header/section-header.component';
 
 @Component({
   selector: 'app-advertise2',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SectionHeaderComponent],
   templateUrl: './advertise2.component.html',
-  styleUrls: ['./advertise2.component.scss']
+  styleUrls: ['./advertise2.component.scss'],
 })
-export class Advertise2Component {
-
-}
+export class Advertise2Component {}
